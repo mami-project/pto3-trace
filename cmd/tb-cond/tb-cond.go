84473f85e8214045c464b086134a5040a4edeba4
Stephan Neuhaus <stephan.neuhaus@zhaw.ch>
2018-04-16 16:12:04 +0200

    Better progress reporting

--- cmd/tb-cond/tb-cond.go
+++ cmd/tb-cond/tb-cond.go
@@ -4,23 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"sort"
-	"sync"
 	"syscall"
+	"time"
 )
 
 type tbStat struct {
 	Count uint64 // how many instances were observed
 }
 
+type stats struct {
+	Conditions     map[string]*tbStat
+	FilesProcessed uint
+	NFiles         uint
+	TimeElapsed    float64 // in seconds
+}
+
+func newStats() *stats {
+	return &stats{
+		Conditions: make(map[string]*tbStat),
+	}
+}
+
 var (
 	nWorkers = flag.Int("workers", 1, "number of workers in pool")
 )
 
-var conditions = make(map[string]*tbStat)
-var lock sync.Mutex
+var conditions = newStats()
 
 type job struct {
 	Path string
@@ -55,8 +68,8 @@ func unmapFile(bytes []byte) error {
 	return syscall.Munmap(bytes)
 }
 
-func processFile(path string) {
-	fmt.Println(path, "started")
+func processFile(path string, stats chan<- stats) {
+	var stat = newStats()
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -73,13 +86,13 @@ func processFile(path string) {
 	matches := ipTCPRe.FindAll(bytes, -1)
 	for _, match := range matches {
 		matchs := string(match)
-		lock.Lock()
-		if conditions[matchs] == nil {
-			conditions[matchs] = new(tbStat)
+		if stat.Conditions[matchs] == nil {
+			stat.Conditions[matchs] = new(tbStat)
 		}
-		conditions[matchs].Count++
-		lock.Unlock()
+		stat.Conditions[matchs].Count++
 	}
+
+	stats <- *stat
 
 	if err := unmapFile(bytes); err != nil {
 		log.Printf("ERROR: can't unmap \"%s\": %v", path, err)
@@ -87,35 +100,90 @@ func processFile(path string) {
 	if err := f.Close(); err != nil {
 		log.Printf("ERROR: can't close \"%s\": %v", path, err)
 	}
-	fmt.Println(path, "done")
 }
 
-func worker(id int, jobs <-chan job, done chan<- bool) {
+func worker(id int, jobs <-chan job, wstats chan<- stats, done chan<- bool) {
 	for job := range jobs {
-		processFile(job.Path)
+		processFile(job.Path, wstats)
 	}
 	done <- true
 }
 
-func processFiles(paths []string) {
-	jobs := make(chan job, 100)
-	done := make(chan bool, *nWorkers)
+func processStats(n uint, wstats <-chan stats, quit <-chan bool, statc chan<- stats) {
+	var conditions = newStats()
+	conditions.NFiles = n
 
-	for w := 1; w <= *nWorkers; w++ {
-		go worker(w, jobs, done)
+	for {
+		select {
+		case stat := <-wstats:
+			for k, v := range stat.Conditions {
+				if conditions.Conditions[k] == nil {
+					conditions.Conditions[k] = new(tbStat)
+				}
+				conditions.Conditions[k].Count += v.Count
+			}
+			conditions.FilesProcessed++
+
+		case statc <- *conditions:
+
+		case <-quit:
+			break
+		}
 	}
+}
 
+func fillJobs(paths []string, jobs chan<- job) {
 	for _, p := range paths {
 		jobs <- job{Path: p}
 	}
 	close(jobs)
+}
+
+func processFiles(paths []string) *stats {
+	jobs := make(chan job, 2*(*nWorkers))
+	done := make(chan bool, *nWorkers)
+	workerStats := make(chan stats, 2*(*nWorkers))
+	accumulatedStats := make(chan stats)
+	squit := make(chan bool)
+
+	go processStats(uint(len(paths)), workerStats, squit, accumulatedStats)
+
+	go fillJobs(paths, jobs)
+
+	for w := 1; w <= *nWorkers; w++ {
+		go worker(w, jobs, workerStats, done)
+	}
+
+	const delay = 1
+	ticker := time.Tick(delay * time.Second)
+	workersDone := false
+	var ret = newStats()
+	var nSec uint
+
+	for !workersDone {
+		select {
+		case <-ticker:
+			nSec += delay
+			*ret = <-accumulatedStats
+			workersDone = ret.FilesProcessed == ret.NFiles
+			frac := float64(ret.FilesProcessed) / float64(ret.NFiles)
+			fmt.Printf("\r%d/%d = %.2f%% done, elapsed = %d sec, ETA = %g sec",
+				ret.FilesProcessed, ret.NFiles,
+				100.0*frac, nSec, math.Round((1.0-frac)*float64(nSec)/frac))
+		}
+	}
+	fmt.Println()
 
 	for w := 1; w <= *nWorkers; w++ {
 		<-done
 	}
+
+	ret.TimeElapsed = float64(nSec)
+
+	return ret
 }
 
-func keys() []string {
+func keys(conditions map[string]*tbStat) []string {
 	keys := make([]string, len(conditions))
 
 	i := 0
@@ -126,8 +194,8 @@ func keys() []string {
 	return keys
 }
 
-func printMap() {
-	ckeys := keys()
+func printMap(conditions map[string]*tbStat) {
+	ckeys := keys(conditions)
 
 	sort.Slice(ckeys, func(i, j int) bool {
 		return conditions[ckeys[i]].Count > conditions[ckeys[j]].Count
@@ -140,6 +208,7 @@ func printMap() {
 
 func main() {
 	flag.Parse()
-	processFiles(flag.Args())
-	printMap()
+	s := processFiles(flag.Args())
+	fmt.Println(s.FilesProcessed, "files in", s.TimeElapsed, "seconds")
+	printMap(s.Conditions)
 }
