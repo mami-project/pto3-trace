ea4e50ba1d1a21984b5033b2bc302dd8bec91a16
Stephan Neuhaus <stephan.neuhaus@zhaw.ch>
2018-05-29 16:55:50 +0200

    Fixed generate for mkmeta

--- cmd/mkmeta/pto3-trace-mkmeta.go
+++ cmd/mkmeta/pto3-trace-mkmeta.go
@@ -2,8 +2,6 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-//go:generate perl ../../gitref.pl gitref.go
-
 package main
 
 import (
